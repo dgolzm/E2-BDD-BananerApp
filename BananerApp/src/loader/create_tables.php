bb7c6aae05e30ed34c819f0cd665c6da5149db12
dgolzm <dclizama@uc.cl>
2024-10-11 17:35:30 -0300

    up

--- BananerApp/src/loader/create_tables.php
+++ BananerApp/src/loader/create_tables.php
@@ -8,37 +8,11 @@ foreach($tablas_iniciales as $tabla => $atributos) {
         echo "Creando tabla $tabla...\n";
         $db->beginTransaction();
         $createTableQuery = "CREATE TABLE IF NOT EXISTS $tabla ($atributos);";
-        $db->exec(statement: $createTableQuery);
+        $db->exec($createTableQuery);
         $db->commit();
     } catch (Exception $e) {
         $db->rollBack();
         echo "Error al crear la tabla $tabla: " . $e->getMessage();
-    }
-}
-
-// Cargar los datos desde los archivos CSV
-foreach($path_tablas as $tabla => $filePath) {
-    try {
-        echo "Cargando datos en la tabla $tabla desde $filePath...\n";
-        $db->beginTransaction();
-        
-        // Leer el archivo CSV
-        if (($handle = fopen(filename: $filePath, mode: "r")) !== FALSE) {
-            $header = fgetcsv(stream: $handle, length: 1000, separator: ",");
-            while (($data = fgetcsv(stream: $handle, length: 1000, separator: ",")) !== FALSE) {
-                $values = array_map(callback: function($value) use ($db): mixed {
-                    return $db->quote($value);
-                }, array: $data);
-                $insertQuery = "INSERT INTO $tabla (" . implode(separator: ", ", array: $header) . ") VALUES (" . implode(separator: ", ", array: $values) . ");";
-                $db->exec(statement: $insertQuery);
-            }
-            fclose(stream: $handle);
-        }
-
-        $db->commit();
-    } catch (Exception $e) {
-        $db->rollBack();
-        echo "Error al cargar datos en la tabla $tabla: " . $e->getMessage();
     }
 }
 ?>
